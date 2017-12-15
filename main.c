ea3aee6bb699d18c6ee73697469796b0ec8535d4
Batu <batuhan.kindan@gmail.com>
2017-12-15 17:04:50 +0300

    KEIL proje dosyaları oluşturuldu, projeye main.c dosyası eklendi

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Batuhan KINDAN
+  * @version V1.0.0
+  * @date    15 Aralik 2017
+  * @brief   Program giris noktasini olusturan main dosyasi  
+  ******************************************************************************
+  */
+
+#include "stm32f4xx.h"                  												// Device header
+
+int main()
+{
+	
+	SystemInit();
+	
+	while(1)
+	{
+		
+	}
+	
+}
